8bcce93f3428d63dda3a50f25820863c16e14020
Xu Kai <kaixu@codeaurora.org>
2013-10-17 16:46:06 +0800

    msm: camera: support simple led flash controlled by one gpio for SKUAB
    SKUAB uses a low cost led for camera flash, the led is driven by
    system power, and use a transistor controlled by external pin to
    gate its on/off state. This is also a single mode flash led with
    no difference between flash mode and torch mode. So we need to add
    support for this in camera driver.
    
    CRs-fixed: 561538
    Change-Id: Ia0c9f6a33e404457c018a719d68fe5f7a6f22029
    Signed-off-by: Xu Kai <kaixu@codeaurora.org>

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -53,6 +53,12 @@
 #define MAX_AF_ITERATIONS 3
 #define MAX_NUMBER_OF_STEPS 47
 
+enum flash_type {
+	LED_FLASH = 1,
+	STROBE_FLASH,
+	GPIO_FLASH
+};
+
 enum msm_camera_i2c_reg_addr_type {
 	MSM_CAMERA_I2C_BYTE_ADDR = 1,
 	MSM_CAMERA_I2C_WORD_ADDR,
