4318dfb546326e1818e05d0dcee4b5bc0321d033
Devdutt Patnaik <dpatnaik@codeaurora.org>
2015-05-04 22:05:07 -0700

    usb: xhci: Set interrupt moderation for host mode to 4000(1ms)
    Higher value allows xhci core to moderate interrupts resulting
    in fewer interrupts from xhci core. This results in lower CPU
    utilization during peak throughput scenarios.
    
    Change-Id: I69548fd9a1adff1b8eafee40c0f92639efd93b2e
    Signed-off-by: Devdutt Patnaik <dpatnaik@codeaurora.org>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -35,6 +35,8 @@
 
 #define	PORT_WAKE_BITS	(PORT_WKOC_E | PORT_WKDISC_E | PORT_WKCONN_E)
 
+#define XHCI_INT_MODERATION_VAL 4000
+
 /* Some 0.95 hardware can't handle the chain bit on a Link TRB being cleared */
 static int link_quirk;
 module_param(link_quirk, int, S_IRUGO | S_IWUSR);
@@ -671,7 +673,7 @@ int xhci_run(struct usb_hcd *hcd)
 	xhci_dbg(xhci, "// Set the interrupt modulation register\n");
 	temp = xhci_readl(xhci, &xhci->ir_set->irq_control);
 	temp &= ~ER_IRQ_INTERVAL_MASK;
-	temp |= (u32) 160;
+	temp |= (u32) XHCI_INT_MODERATION_VAL;
 	xhci_writel(xhci, temp, &xhci->ir_set->irq_control);
 
 	/* Set the HCD state before we enable the irqs */
