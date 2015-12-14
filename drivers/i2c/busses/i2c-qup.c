39026915dcd31adb07e49daa31973135654375a4
eray orçunus <erayorcunus@gmail.com>
2015-12-14 23:21:09 +0200

    i2c-qup: Translate controller errors to real ones
    Author: Kiran Gunda

--- drivers/i2c/busses/i2c-qup.c
+++ drivers/i2c/busses/i2c-qup.c
@@ -1227,7 +1227,7 @@ timeout_err:
 				} else if (dev->err < 0) {
 					dev_err(dev->dev,
 					"QUP data xfer error %d\n", dev->err);
-					ret = dev->err;
+					ret = -EIO;
 					goto out_err;
 				} else if (dev->err > 0) {
 					/*
@@ -1238,7 +1238,7 @@ timeout_err:
 					 */
 					qup_i2c_recover_bus_busy(dev);
 				}
-				ret = -dev->err;
+				ret = -EBUSY;
 				goto out_err;
 			}
 			if (dev->msg->flags & I2C_M_RD) {
