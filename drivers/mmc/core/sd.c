5f7e8ba33daa57cf8a3d58a5ef051446f35e8583
eray orçunus <erayorcunus@gmail.com>
2016-01-12 17:37:40 +0200

    mmc: core: update host->card after getting RCA for SD card
    During tuning the status command CMD13 needs to be sent to the
    card to know the card's state upon any failure to tuning command.
    
    Change-Id: Iaefc80305d101bd72ff22f792b1967379507a739
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1040,6 +1040,7 @@ static int mmc_sd_init_card(struct mmc_host *host, u32 ocr,
 		err = mmc_send_relative_addr(host, &card->rca);
 		if (err)
 			return err;
+		host->card = card;
 	}
 
 	if (!oldcard) {
@@ -1099,12 +1100,13 @@ static int mmc_sd_init_card(struct mmc_host *host, u32 ocr,
 		}
 	}
 
-	host->card = card;
 	return 0;
 
 free_card:
-	if (!oldcard)
+	if (!oldcard) {
+		host->card = NULL;
 		mmc_remove_card(card);
+	}
 
 	return err;
 }
