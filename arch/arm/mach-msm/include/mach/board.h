4d2e68c8fb4deabfec9f10d10043e5be73c19ac7
eray orçunus <erayorcunus@gmail.com>
2016-02-06 03:29:05 +0200

    3.4 wcnss port fix & prepare for some camera commit

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -35,6 +35,12 @@
 #define RF_TYPE_48 0x30
 #define RF_TYPE_49 0x31
 
+#define WLAN_RF_REG_ADDR_START_OFFSET   0x3
+#define WLAN_RF_REG_DATA_START_OFFSET   0xf
+#define WLAN_RF_READ_REG_CMD            0x3
+#define WLAN_RF_WRITE_REG_CMD           0x2
+#define WLAN_RF_READ_CMD_MASK           0x3fff
+
 struct msm_camera_io_ext {
 	uint32_t mdcphy;
 	uint32_t mdcsz;
@@ -191,8 +197,7 @@ struct msm_gpio_set_tbl {
 };
 
 struct msm_camera_gpio_num_info {
-	uint16_t gpio_num[10];
-	uint8_t valid[10];
+	uint16_t gpio_num[7];
 };
 
 struct msm_camera_gpio_conf {
@@ -692,4 +697,5 @@ int smd_debugfs_init(void);
 static inline int smd_debugfs_init(void) { return 0; }
 #endif
 
+u32 wcnss_rf_read_reg(u32 rf_reg_addr);
 #endif
