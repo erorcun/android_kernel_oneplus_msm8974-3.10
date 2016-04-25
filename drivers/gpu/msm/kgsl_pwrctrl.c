9f1361650ba2cbbe673d28147a7baf640aaa5132
eray orçunus <erayorcunus@gmail.com>
2016-04-25 17:03:37 +0300

    msm: kgsl: Fix direct references to HZ
    Make the various timeout values HZ agnostic by using the proper
    macros and values instead.
    
    Change-Id: I906b948657c8873518042c7465272c98c5391e59
    Signed-off-by: Suman Tatiraju <sumant@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -471,7 +471,7 @@ static ssize_t kgsl_pwrctrl_idle_timer_store(struct device *dev,
 {
 	unsigned int val = 0;
 	struct kgsl_device *device = kgsl_device_from_dev(dev);
-	const long div = 1000/HZ;
+	const long div = 1000/msecs_to_jiffies(1000);
 	int ret;
 
 	if (device == NULL)
@@ -506,7 +506,7 @@ static ssize_t kgsl_pwrctrl_idle_timer_show(struct device *dev,
 					char *buf)
 {
 	struct kgsl_device *device = kgsl_device_from_dev(dev);
-	int mul = 1000/HZ;
+	int mul = 1000/msecs_to_jiffies(1000);
 	if (device == NULL)
 		return 0;
 	/* Show the idle_timeout converted to msec */
@@ -1135,7 +1135,7 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 
 	pwr->power_flags = 0;
 
-	pwr->interval_timeout = pdata->idle_timeout;
+	pwr->interval_timeout = msecs_to_jiffies(pdata->idle_timeout);
 	pwr->strtstp_sleepwake = pdata->strtstp_sleepwake;
 
 	pwr->pm_qos_active_latency = pdata->pm_qos_active_latency;
@@ -1705,7 +1705,7 @@ int kgsl_active_count_wait(struct kgsl_device *device, int count)
 		int ret;
 		kgsl_mutex_unlock(&device->mutex, &device->mutex_owner);
 		ret = wait_event_timeout(device->active_cnt_wq,
-			_check_active_count(device, count), HZ);
+			_check_active_count(device, count), msecs_to_jiffies(1000));
 		kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
 		result = ret == 0 ? -ETIMEDOUT : 0;
 	}
