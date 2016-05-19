345ade2015921be6d1cb6dd7597febbeba23687b
eray orçunus <erayorcunus@gmail.com>
2016-05-19 18:46:02 +0300

    sound: Merge fix

--- sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.h
+++ sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.h
@@ -108,7 +108,6 @@ struct msm_audio {
 	int cmd_interrupt;
 	bool meta_data_mode;
 	uint32_t volume;
-	struct msm_audio_in_frame_info *in_frame_info; /* array of frame info */
 	/* array of frame info */
 	struct msm_audio_in_frame_info in_frame_info[CAPTURE_MAX_NUM_PERIODS];
 };
