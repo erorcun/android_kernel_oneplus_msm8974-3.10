2c1fcd5bdf19d099e80bc7abe4d22fff3e121bcc
eray orçunus <erayorcunus@gmail.com>
2016-06-28 11:50:48 +0300

    mm: fix anon vma naming
    Fix two bugs caused by merging anon vma_naming, a typo in
    mempolicy.c and a bad merge in sys.c.
    
    Change-Id: Ia4ced447d50573e68195e95ea2f2b4d9456b8a90
    Signed-off-by: Colin Cross <ccross@android.com>
    Git-commit: 527aae529118f2cbe99c945ffe74be1c04ceaba6
    Git-Repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Ian Maund <imaund@codeaurora.org>

--- kernel/sys.c
+++ kernel/sys.c
@@ -2389,9 +2389,6 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 			put_task_struct(tsk);
 			error = 0;
 			break;
-		case PR_SET_VMA:
-			error = prctl_set_vma(arg2, arg3, arg4, arg5);
-			break;
 		default:
 			return -EINVAL;
 		}
@@ -2428,6 +2425,9 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 		if (arg2 || arg3 || arg4 || arg5)
 			return -EINVAL;
 		return task_no_new_privs(current) ? 1 : 0;
+	case PR_SET_VMA:
+		error = prctl_set_vma(arg2, arg3, arg4, arg5);
+		break;
 	default:
 		error = -EINVAL;
 		break;
