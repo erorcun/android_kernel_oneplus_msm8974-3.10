d9f445a7e415397556802bfc362f04b85116217e
eray orçunus <erayorcunus@gmail.com>
2016-07-03 21:32:30 +0300

    Revert "cpuidle: Expand the scope of CPU specific QoS"
    This reverts commit a4391f9188be5bb4ca09d977351e14557a9aaf48.

--- drivers/cpuidle/governors/ladder.c
+++ drivers/cpuidle/governors/ladder.c
@@ -69,7 +69,7 @@ static int ladder_select_state(struct cpuidle_driver *drv,
 	struct ladder_device *ldev = &__get_cpu_var(ladder_devices);
 	struct ladder_device_state *last_state;
 	int last_residency, last_idx = ldev->last_state_idx;
-	int latency_req = pm_qos_request_for_cpu(PM_QOS_CPU_DMA_LATENCY, dev->cpu);
+	int latency_req = pm_qos_request(PM_QOS_CPU_DMA_LATENCY);
 
 	/* Special case when user has set very strict latency requirement */
 	if (unlikely(latency_req == 0)) {
