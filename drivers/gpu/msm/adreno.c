b32d29be6019e79f6de603627592c9e05d579c7b
eray orçunus <erayorcunus@gmail.com>
2016-08-26 14:08:06 +0300

    msm: kgsl: Replace device->parentdev with device->pdev
    The code spends a lot of time converting device->parentdev
    from a device pointer to a platform_device and then back into
    a device again. Cut through the middle man and track just the
    platform_device pointer in kgsl_device.
    
    Change-Id: Ic0dedbada14e2adaee754bbfedc4ca33087c446c
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1257,7 +1257,7 @@ static unsigned int
 adreno_getchipid(struct kgsl_device *device)
 {
 	struct kgsl_device_platform_data *pdata =
-		kgsl_device_get_drvdata(device);
+		dev_get_platdata(&device->pdev->dev);
 
 	/* All A3XX and newer chipsets will specify the chipid in pdata */
 	return pdata->chipid;
@@ -1656,7 +1656,7 @@ adreno_probe(struct platform_device *pdev)
 
 	device = (struct kgsl_device *)pdev->id_entry->driver_data;
 	adreno_dev = ADRENO_DEVICE(device);
-	device->parentdev = &pdev->dev;
+	device->pdev = pdev;
 
 	status = kgsl_device_platform_probe(device);
 	if (status)
@@ -1694,7 +1694,7 @@ error_close_rb:
 error_close_device:
 	kgsl_device_platform_remove(device);
 error:
-	device->parentdev = NULL;
+	device->pdev = NULL;
 error_return:
 	return status;
 }
