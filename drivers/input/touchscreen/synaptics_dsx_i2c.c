78d96bad0a414c59d1e6d9a156affebee10abfb8
eray orçunus <erayorcunus@gmail.com>
2016-09-04 14:59:33 +0300

    prevent_bl: Fixes
    * I don't get fb status types yet, current code acts abnormally.
    * Removes gesture based detection and just lights up backlight on
      unblank fb event.
    * Reverts b9f468aa.

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -1996,9 +1996,7 @@ hw_shutdown:
 
 #ifdef CONFIG_DONT_LIGHT_LED_ON_TOUCH
 extern int prevent_bl;
-extern void disable_bttn_bl(void);
 extern void enable_bttn_bl(void);
-int vsync_suspend = 0;
 #endif
 
 static unsigned char synaptics_rmi4_update_gesture2(unsigned char *gesture,
@@ -2023,9 +2021,6 @@ static unsigned char synaptics_rmi4_update_gesture2(unsigned char *gesture,
 			gesturemode = Circle;
 			if (atomic_read(&syna_rmi4_data->camera_enable)) {
 				keyvalue = KEY_GESTURE_CIRCLE;
-#ifdef CONFIG_DONT_LIGHT_LED_ON_TOUCH
-				prevent_bl = 2;
-#endif
 			}
 			break;
 
@@ -2053,9 +2048,6 @@ static unsigned char synaptics_rmi4_update_gesture2(unsigned char *gesture,
 			gesturemode = DouTap;
 			if (atomic_read(&syna_rmi4_data->double_tap_enable)) {
 				keyvalue = KEY_WAKEUP;
-#ifdef CONFIG_DONT_LIGHT_LED_ON_TOUCH
-				prevent_bl = 2;
-#endif
 			}
 			break;
 
@@ -2283,7 +2275,7 @@ static int synaptics_rmi4_f12_abs_report(struct synaptics_rmi4_data *rmi4_data,
 	}
 
 #ifdef CONFIG_DONT_LIGHT_LED_ON_TOUCH
-	if(!prevent_bl && !vsync_suspend && !rmi4_data->old_status)
+	if(!prevent_bl)
 		enable_bttn_bl();
 #endif
 
@@ -4420,7 +4412,6 @@ static int fb_notifier_callback(struct notifier_block *p,
 	switch (event) {
 		case FB_EVENT_BLANK :
 			ev = (*(int *)evdata->data);
-			vsync_suspend = 0;
 
 			/*
 			 * Normal Screen Wakeup
@@ -4436,23 +4427,20 @@ static int fb_notifier_callback(struct notifier_block *p,
 			 */
 			switch (ev) {
 				/* Screen On */
-				case FB_BLANK_UNBLANK: // 0
-				case FB_BLANK_NORMAL: // 1
-				case FB_BLANK_HSYNC_SUSPEND: // 3
+				case FB_BLANK_UNBLANK:
+				case FB_BLANK_NORMAL:
+					prevent_bl = 0;
+					enable_bttn_bl();
+				case FB_BLANK_VSYNC_SUSPEND:
+				case FB_BLANK_HSYNC_SUSPEND:
 					new_status = 0;
-					break;
-				case FB_BLANK_VSYNC_SUSPEND: // 2
-					new_status = 0;
-					vsync_suspend = 1;
-					disable_bttn_bl();
 					break;
 				default:
 					/* Default to screen off to match previous
 					   behaviour */
 					print_ts(TS_INFO, KERN_INFO "[syna] Unhandled event %i\n", ev);
 					/* Fall through */
-				case FB_BLANK_POWERDOWN: // 4
-					disable_bttn_bl();
+				case FB_BLANK_POWERDOWN:
 					new_status = 1;
 					break;
 			}
