7a876815143ce65802caa13c5302fb221cd0a854
eray orçunus <erayorcunus@gmail.com>
2017-03-01 02:18:32 +0300

    cpu-boost: Revert the values I've changed
    * Disable load_based_sync and restore migration_load_threshold
    * I went crazy due to that in this week. There was no error on our build,
      and sync_threshold is actually MAX. freq for migration boost freq, so it's
      a good thing.
    
    Signed-off-by: eray orçunus <erayorcunus@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -58,10 +58,10 @@ module_param(input_boost_freq, uint, 0644);
 static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
 
-static unsigned int migration_load_threshold = 25;
+static unsigned int migration_load_threshold = 15;
 module_param(migration_load_threshold, uint, 0644);
 
-static bool load_based_syncs = true;
+static bool load_based_syncs = false;
 module_param(load_based_syncs, bool, 0644);
 
 static u64 last_input_time;
